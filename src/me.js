17fbd9742e07e6da6ef4ee7a31b0d371578056a4
spettenn <69841778+spettenn@users.noreply.github.com>
2022-10-28 14:47:53 +0200

    updates

--- src/me.js
+++ src/me.js
@@ -1,25 +1,21 @@
 const header = {
-	// all the properties are optional - can be left empty or deleted
 	homepage: 'https://rajshekhar26.github.io/cleanfolio',
 	title: 'JS.',
 };
 
 const about = {
-	// all the properties are optional - can be left empty or deleted
-	name: 'John Smith',
-	role: 'Front End Engineer',
+	name: 'Aleksander Spetalen',
+	role: 'Front-End Developer',
 	description:
-		'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-	resume: 'https://example.com',
+		'I am a front-end developer from Oslo (Norway), currently i am working for a headhunting firm developing the website and CMS. I am passionate about web development and i am always looking for new challenges.',
+
 	social: {
-		linkedin: 'https://linkedin.com',
-		github: 'https://github.com',
+		linkedin: 'https://www.linkedin.com/in/aleksander-spetalen-450013209/',
+		github: 'https://github.com/spettenn',
 	},
 };
 
 const projects = [
-	// projects can be added an removed
-	// if there are no projects, Projects section won't show up
 	{
 		name: 'Project 1',
 		description:
@@ -47,25 +43,19 @@ const projects = [
 ];
 
 const skills = [
-	// skills can be added or removed
-	// if there are no skills, Skills section won't show up
 	'HTML',
 	'CSS',
 	'JavaScript',
 	'TypeScript',
 	'React',
-	'Redux',
 	'SASS',
 	'Material UI',
 	'Git',
-	'CI/CD',
 	'Jest',
-	'Enzyme',
 ];
 
 const contact = {
-	// email is optional - if left empty Contact section won't show up
-	email: 'johnsmith@mail.com',
+	email: 'Aleksanderspetalen@gmail.com',
 };
 
 export { header, about, projects, skills, contact };
